76895e0fdb991a80c2bfbdf8d4681c6321700fa7
DESKTOP-IGKV12I\Administrator <korcstar@gmail.com>
2021-08-22 04:58:49 +0800

    Add frontend production to integrate with frontend

---
+++ app/views/precache-manifest.444dfd87aa56adba485c254dcf3357f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241a5adbf6587225e61615a6a77f82f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "593702bd67968d68c2aa",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "073524eb2e1c737f3975",
+    "url": "/static/css/main.9bab6a0f.chunk.css"
+  },
+  {
+    "revision": "593702bd67968d68c2aa",
+    "url": "/static/js/2.b6293b4a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.b6293b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073524eb2e1c737f3975",
+    "url": "/static/js/main.c4133eb8.chunk.js"
+  },
+  {
+    "revision": "b8b3634781205923e9c2",
+    "url": "/static/js/runtime-main.c77cb077.js"
+  }
+]);
